eadd917834dc97d0a2e62e06e5a2a50f2e798da6
mariamayman <mayman0901@gmail.com>
2022-02-25 22:31:59 +0200

    author page UI

---
+++ app/src/main/java/com/example/shahry_blog/presentation/AuthorPageFragment.kt
@@ -0,0 +1,28 @@
+package com.example.shahry_blog.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.shahry_blog.R
+import com.example.shahry_blog.databinding.FragmentAuthorPageBinding
+import com.example.shahry_blog.helpers.bind
+
+class AuthorPageFragment : Fragment() {
+    lateinit var binding: FragmentAuthorPageBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return bind<FragmentAuthorPageBinding>(
+            R.layout.fragment_author_page,
+            inflater,
+            container
+        ) { bind ->
+            this.binding = bind
+
+        }
+    }
+}
